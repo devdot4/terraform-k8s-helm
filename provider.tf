967c736db7f23526fe465554395bfcb101283c7e
devdot4 <15527358+devdot4@users.noreply.github.com>
2026-08-08 20:00:24 +0000

    add first resource

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
-# terraform-k8s-helm
-
-# add this block of code to provider.tf
-```
 provider "helm" {
   kubernetes = {
     config_path = "~/.kube/config"
@@ -20,14 +16,3 @@ provider "helm" {
     }
   ]
 }
-```
-
-# add code below to use:
-```
-module name {
-  source     = "devdot4/helm/k8s"
-  name       = "nginx-ingress-controller"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-}
-```
